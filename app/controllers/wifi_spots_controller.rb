de103f0f6adb08a3004c322758d558d2eeeaa2f4
Yuya Yabe <yuyabee@gmail.com>
2017-05-27 16:13:24 -0700

    Add sample data

--- app/controllers/wifi_spots_controller.rb
+++ app/controllers/wifi_spots_controller.rb
@@ -1,5 +1,6 @@
 class WifiSpotsController < ApplicationController
   def index
+    p WifiSpot.find(1)
     render json: params.permit(:distance, :limit, :lat, :lng), status: :ok
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach("#{Rails.root}/db/jta_free_wifi.csv", {headers: true}) do |r|
+  spot = WifiSpot.new(name_jp: r[1], name_en: r[2], address_jp: r[8], address_en: r[9], latitude: r[18], longitude: r[19])
+  spot.save!
+end
