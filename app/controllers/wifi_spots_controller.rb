d66c00e7f4782622d8ad0cb1d0e5f06d1acbb2cf
Yuya Yabe <yuyabee@gmail.com>
2017-05-27 17:04:20 -0700

    Add limit feature

--- app/controllers/wifi_spots_controller.rb
+++ app/controllers/wifi_spots_controller.rb
@@ -2,7 +2,7 @@ class WifiSpotsController < ApplicationController
   before_action :set_params, only: [:index]
 
   def index
-    res = WifiSpot.near([@lat, @lng], @distance / 1000, :unit => :km)
+    res = WifiSpot.near([@lat, @lng], @distance / 1000, :unit => :km).limit(@limit)
 
     render json: res, status: :ok
   end
@@ -14,6 +14,7 @@ class WifiSpotsController < ApplicationController
 
     @lat = params.fetch(:lat).to_f
     @lng = params.fetch(:lng).to_f
+    @limit = params.fetch(:limit, 5).to_i
     @distance = params.fetch(:distance, 500).to_i # unit: meter
   end
 end
