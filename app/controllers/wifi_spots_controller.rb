1cfa900a6b303fdc1095d388447082f8af46a989
Yuya Yabe <yuyabee@gmail.com>
2017-05-27 17:00:51 -0700

    Implement search by lat, lng, distance

--- app/controllers/wifi_spots_controller.rb
+++ app/controllers/wifi_spots_controller.rb
@@ -1,6 +1,19 @@
 class WifiSpotsController < ApplicationController
+  before_action :set_params, only: [:index]
+
   def index
-    p WifiSpot.find(1)
-    render json: params.permit(:distance, :limit, :lat, :lng), status: :ok
+    res = WifiSpot.near([@lat, @lng], @distance / 1000, :unit => :km)
+
+    render json: res, status: :ok
+  end
+
+  private
+
+  def set_params
+    params.permit(:lat, :lng, :distance, :limit)
+
+    @lat = params.fetch(:lat).to_f
+    @lng = params.fetch(:lng).to_f
+    @distance = params.fetch(:distance, 500).to_i # unit: meter
   end
 end
