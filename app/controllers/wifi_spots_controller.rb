64a63902fc8fa8dd486285de097f6b02be8a047f
Yuya Yabe <yuyabee@gmail.com>
2017-05-28 13:38:06 -0700

    Add distance from to response

--- app/controllers/wifi_spots_controller.rb
+++ app/controllers/wifi_spots_controller.rb
@@ -2,7 +2,11 @@ class WifiSpotsController < ApplicationController
   before_action :set_params, only: [:index]
 
   def index
-    res = WifiSpot.near([@lat, @lng], @distance / 1000, :unit => :km).limit(@limit)
+    res = WifiSpot
+      .near(@geo_point, @distance / 1000)
+      .limit(@limit)
+      .map{|s| s.as_json.merge({ "distance" => (s.distance_from(@geo_point) * 1000).to_i })}
+
 
     render json: res, status: :ok
   end
@@ -12,8 +16,7 @@ class WifiSpotsController < ApplicationController
   def set_params
     params.permit(:lat, :lng, :distance, :limit)
 
-    @lat = params.fetch(:lat).to_f
-    @lng = params.fetch(:lng).to_f
+    @geo_point = [params.fetch(:lat).to_f, params.fetch(:lng).to_f]
     @limit = params.fetch(:limit, 5).to_i
     @distance = params.fetch(:distance, 500).to_i # unit: meter
   end
