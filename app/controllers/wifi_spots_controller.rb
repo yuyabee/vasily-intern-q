ba63ec08d198737c770e815885d503ecd302f2c8
Yuya Yabe <yuyabee@gmail.com>
2017-05-28 16:13:15 -0700

    Add lang params

--- app/controllers/wifi_spots_controller.rb
+++ app/controllers/wifi_spots_controller.rb
@@ -2,10 +2,21 @@ class WifiSpotsController < ApplicationController
   before_action :set_params, only: [:index]
 
   def index
+    attrs = ["distance",  "latitude", "longitude"]
+    case @lang
+    when "ja"
+      attrs += ["name_jp", "address_jp"]
+    when "en"
+      attrs += ["name_en", "address_en"]
+    else
+      attrs += ["name_jp", "address_jp", "name_en", "address_en"]
+    end
+
     res = WifiSpot
       .near(@geo_point, @distance / 1000)
       .limit(@limit)
-      .map{|s| s.as_json.merge({ "distance" => (s.distance_from(@geo_point) * 1000).to_i })}
+      .map {|s| s.as_json.merge({ "distance" => (s.distance_from(@geo_point) * 1000).to_i })}
+      .map {|s| s.slice(*attrs)}
 
     render json: res, status: :ok
   end
@@ -13,12 +24,14 @@ class WifiSpotsController < ApplicationController
   private
 
   def set_params
-    params.permit(:lat, :lng, :distance, :limit)
+    params.permit(:lat, :lng, :distance, :limit, :lang)
 
     @geo_point = [params.fetch(:lat).to_f, params.fetch(:lng).to_f]
 
     # setting default when the params not given
     @limit = params.fetch(:limit, 5).to_i
     @distance = params.fetch(:distance, 500).to_i # unit: meter
+
+    @lang = params.fetch(:lang, nil)
   end
 end
