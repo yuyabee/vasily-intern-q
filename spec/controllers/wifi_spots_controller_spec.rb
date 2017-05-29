2164d045a085d76e8e2c55869c4d13a4bd281e13
Yuya Yabe <yuyabee@gmail.com>
2017-05-28 20:00:20 -0700

    Add test for search parameter

--- spec/controllers/wifi_spots_controller_spec.rb
+++ spec/controllers/wifi_spots_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe WifiSpotsController, type: :controller do
     let(:lang) { "en" }
     let(:distance) { 1000 }
     let(:limit) { 15 }
+    let(:search) { "新宿" }
 
     it "assigns default values to @limit, @distance and @lang" do
       get :index, params: { lat: lat, lng: lng }
@@ -20,6 +21,18 @@ RSpec.describe WifiSpotsController, type: :controller do
       get :index, params: { lat: lat, lng: lng }
 
       expect(assigns(:geo_point)).to eq([lat, lng])
+    end
+
+    it "assigns params[:search] to @geo_point" do
+      get :index, params: { search: search }
+
+      expect(assigns(:geo_point)).to eq(search)
+    end
+
+    it "assigns params[:search] to @geo_point when [lat, lng] and search is given" do
+      get :index, params: { lat: lat, lng: lng, search: search }
+
+      expect(assigns(:geo_point)).to eq(search)
     end
 
     it "assigns @limit, @distance, and @lang with the given parameters" do
